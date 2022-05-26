044b8c10ec5486f7d37126abd1b9e8007e6ea1d8
NotTimIsReal <alastairrintumathew@gmail.com>
2022-05-26 18:42:29 +1000

    ncurses update

--- ncurses.py
+++ ncurses.py
@@ -1,8 +1,11 @@
 # Created With Barrell Tool https://ferment.tk/create
-import subprocess
-from index import Barrells
 import os
+import subprocess
 import sys
+
+from index import Barrells
+
+
 class ncurses(Barrells):
     def __init__(self):
         self.description = "Ncurses"
@@ -10,39 +13,19 @@ class ncurses(Barrells):
         self.git = False
         self.lib = True
         self.dependencies = ["pkg-config"]
-    
+
     def install(self):
         with open("/tmp/ncurses.log", "a") as sys.stdout:
             os.chdir(self.cwd)
-            args=[f"--prefix={self.cwd}/built", "--enable-pc-files","--with-pkg-config-libdir=/usr/local/lib/pkgconfig", "--enable-sigwinch", "--enable-symlinks", "--enable-widec", "--with-shared",  "--with-cxx-shared","--with-gpm=no",  "--without-ada"]
+            args=[f"--prefix=/usr/local/", "--enable-pc-files","--with-pkg-config-libdir=/usr/local/lib/pkgconfig", "--enable-sigwinch", "--enable-symlinks", "--enable-widec", "--with-shared",  "--with-cxx-shared","--with-gpm=no",  "--without-ada"]
             subprocess.run(["sh", "./configure", *args], stdout=sys.stdout, stderr=sys.stdout)
             subprocess.run(["make"], stdout=sys.stdout, stderr=sys.stdout)
             subprocess.run(["make", "install"], stdout=sys.stdout, stderr=sys.stdout)
-            syms=os.listdir(self.cwd+"/built/bin")
-            for sym in syms:
-                os.symlink(self.cwd+"/built/bin/"+sym, f"/usr/local/bin/{sym}")
-            os.symlink(f"{self.cwd}/built/include/ncursesw", "/usr/local/include/ncurses")
-            syms=os.listdir(self.cwd+"/built/lib")
-            for sym in syms:
-                if(os.path.isdir(sym)):
-                    continue
-                os.symlink(self.cwd+"/built/lib/"+sym, f"/usr/local/lib/{sym}")
-            syms=os.listdir(self.cwd+"/built/lib/pkgconfig")
-            for sym in syms:
-                os.symlink(self.cwd+"/built/lib/pkgconfig/"+sym, f"/usr/local/lib/pkgconfig/{sym}")
             return super().install()
     def uninstall(self):
         try:
-            binsym=os.listdir(f"{self.cwd}/built/bin")
-            libsyms=os.listdir(self.cwd+"/built/lib")
-            pkgsyms=os.listdir(self.cwd+"/built/lib/pkgconfig")
-            os.remove("/usr/local/include/ncursesw")
-            for sym in binsym:
-                os.remove(f"/usr/local/bin/{sym}")
-            for sym in libsyms:
-                os.remove(f"/usr/local/lib/{sym}")
-            for sym in pkgsyms:
-                os.remove(f"/usr/local/lib/pkgconfig/{sym}")
+            os.chdir(self.cwd)
+            subprocess.run(["make", "uninstall"])
         finally:
             return super().uninstall()
-    
+
