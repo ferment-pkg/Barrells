1d17eda1f4a944e7e48008c4aefc2f6bae5ec311
NotTimIsReal <alastairrintumathew@gmail.com>
2022-06-12 17:57:06 +1000

    added uninstall comamnd for pv

--- pv.py
+++ pv.py
@@ -1,5 +1,7 @@
 # Created With Barrell Tool https://ferment.tk/create
 from index import Barrells
+
+
 class pv(Barrells):
     def __init__(self):
         self.description = "Monitor data's progress through a pipe"
@@ -7,14 +9,23 @@ class pv(Barrells):
         self.git = False
         self.lib = False
         self.home = "https://www.ivarch.com/programs/pv.shtml"
-        self.dependencies = ["automake","pkg-config"]
-    
+        self.dependencies = ["automake", "pkg-config"]
+
     def install(self):
-      import subprocess
-      args=["--prefix=/usr/local/", "--disable-nls"]
-      import os
-      os.chdir(self.cwd)
-      subprocess.call(["sh", "./configure", *args])
-      subprocess.call(["make"])
-      subprocess.call(["make", "uninstall"])
-    
+        import subprocess
+
+        args = ["--prefix=/usr/local/", "--disable-nls"]
+        import os
+
+        os.chdir(self.cwd)
+        subprocess.call(["sh", "./configure", *args])
+        subprocess.call(["make"])
+        subprocess.call(["make", "uninstall"])
+
+    def uninstall(self):
+        import os
+
+        os.chdir(self.cwd)
+        import subprocess
+
+        subprocess.call(["make", "uninstall"])
