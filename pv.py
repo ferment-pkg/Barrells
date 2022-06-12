55364c25be99b52d8e397526fea10aead17d6f3c
NotTimIsReal <alastairrintumathew@gmail.com>
2022-06-12 17:46:54 +1000

    added pv

---
+++ pv.py
@@ -0,0 +1,20 @@
+# Created With Barrell Tool https://ferment.tk/create
+from index import Barrells
+class pv(Barrells):
+    def __init__(self):
+        self.description = "Monitor data's progress through a pipe"
+        self.url = "https://github.com/icetee/pv/archive/refs/tags/v1.6.6.tar.gz"
+        self.git = False
+        self.lib = False
+        self.home = "https://www.ivarch.com/programs/pv.shtml"
+        self.dependencies = ["automake","pkg-config"]
+    
+    def install(self):
+      import subprocess
+      args=["--prefix=/usr/local/", "--disable-nls"]
+      import os
+      os.chdir(self.cwd)
+      subprocess.call(["sh", "./configure", *args])
+      subprocess.call(["make"])
+      subprocess.call(["make", "uninstall"])
+    
