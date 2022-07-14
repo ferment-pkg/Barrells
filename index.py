7943dd1aeb59449064819dd8dac6d37eae5cb987
NotTimIsReal <alastairrintumathew@gmail.com>
2022-07-14 17:25:45 +1000

    pkgconfig multi arch build

--- index.py
+++ index.py
@@ -48,6 +48,26 @@ class Barrells:
         os.environ["PKG_CONFIG_PATH"]="/usr/local/lib/pkgconfig"
     def runcmdincwd(self, cmd):
         return subprocess.run(cmd, cwd=self.cwd)
+    def removeTMPWaterMark(self, pkg:str)->None:
+        originalcontent=open("Makefile", "r+").read()
+        while(f"/private/tmp/fermenter/{pkg}" in originalcontent):
+            originalcontent=originalcontent.replace(f"/private/tmp/fermenter/{pkg}", self.cwd+f"/")
+            originalcontent=originalcontent.replace(f"/tmp/fermenter/{pkg}", self.cwd+f"/")
+            originalcontent=originalcontent.replace("install: all install-bin install-hdrs install-lib install-data", "install: install-bin install-hdrs install-lib install-data")
+            open("Makefile", "w").write(originalcontent)
+            originalcontent=open("Makefile", "r+").read()
+        builddir=os.listdir("build")
+        #buildir should only contains files with the .d extention
+        builddir=[x for x in builddir if x.endswith(".d")]
+        for x in builddir:
+            ogcontent=open(f"build/{x}", "r+").read()
+            ogcontent=ogcontent.replace(f"/private/tmp/fermenter/{pkg}", self.cwd+f"/")
+            open("build/"+x, "w").write(ogcontent)
+            ogcontent=open(f"build/{x}", "r+").read()
+        originalcontent=open("libtool", "r+").read()
+        originalcontent=originalcontent.replace(f"/private/tmp/fermenter/{pkg}", self.cwd+f"/")
+        originalcontent=originalcontent.replace(f"/tmp/fermenter/{pkg}", self.cwd+f"/")
+        open("libtool", "w").write(originalcontent)
 class Prebuild():
     def __init__(self):
         self.amd64:str
--- pkgconfig.py
+++ pkgconfig.py
@@ -1,39 +1,52 @@
 #Credit: Kareku Sato - https://noknow.info/it/os/install_pkg_config_from_source?lang=en
-import subprocess
-import requests
 import os
-import platform
-from index import Barrells
+import subprocess
+
+from index import Barrells, Prebuild
+
+
 class pkgconfig(Barrells):
     def __init__(self):
         self.url="https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz"
         self.git=False
         self.description="Manage compile and link flags for libraries"
         self.dependencies=["autoconf", "automake", "libtool"]
+        self.prebuild=prebuild()
     def install(self):
-            os.chdir(self.cwd)
-            args=["--disable-debug", f"--prefix={self.cwd}/built","--disable-host-tool", " --with-internal-glib"]
-            #https://github.com/ferment-pkg/pkg-config-prebuilt
-            if "x86_64" in platform.machine():
-                # Use prebuilt amd64 binary
-                content=requests.get("https://github.com/ferment-pkg/pkg-config-prebuilt/archive/refs/tags/v1.tar.gz").content
-                with open("pkg-config-prebuilt.tar.gz", "wb") as f:
-                    f.write(content)
-                os.system("tar -xzf pkg-config-prebuilt.tar.gz")
-                os.system("mv pkg-config-prebuilt-1 built")
-                os.symlink(f"{self.cwd}/built/amd64/bin/pkg-config", "/usr/local/bin/pkg-config")
-                os.symlink(f"{self.cwd}/built/amd64/share/aclocal/pkg.m4", "/usr/local/share/aclocal/pkg.m4")
-            else: 
-                subprocess.call(["sh","configure", *args])
-                subprocess.call(["make"])
-                subprocess.call(["make","install"])
-                os.symlink(f"{self.cwd}/built/bin/pkg-config", "/usr/local/bin/pkg-config")
-                os.symlink(f"{self.cwd}/built/share/aclocal/pkg.m4", "/usr/local/share/aclocal/pkg.m4")
-            os.mkdir("/usr/local/lib/pkgconfig")
-            os.link()
+        os.chdir(self.cwd)
+        args=["--disable-debug", f"--prefix=/usr/local/","--disable-host-tool", " --with-internal-glib"]
+        env=os.environ.copy()
+        env["CC"]="clang"
+        env["CXX"]="clang++"
+        env["CFLAGS"]="-arch arm64 -arch x86_64"
+        env["CXXFLAGS"]="-arch arm64 -arch x86_64"
+        subprocess.call(["sh","configure", *args], env=env)
+        subprocess.call(["make", f"-j{os.cpu_count()}"], env=env)
+        subprocess.call(["make","install", f"-j{os.cpu_count()}"], env=env)
+        os.mkdir("/usr/local/lib/pkgconfig")
+    def build(self):
+        os.chdir(self.cwd)
+        args=["--disable-debug", f"--prefix=/usr/local/","--disable-host-tool", " --with-internal-glib"]
+        env=os.environ.copy()
+        env["CC"]="clang"
+        env["CXX"]="clang++"
+        env["CFLAGS"]="-arch arm64 -arch x86_64"
+        env["CXXFLAGS"]="-arch arm64 -arch x86_64"
+        self.removeTMPWaterMark("pkgconfig")
+        subprocess.call(["sh","configure", *args], env=env)
+        subprocess.call(["make", f"-j{os.cpu_count()}"], env=env)
+
+
     def uninstall(self) -> bool:
         try:
-            os.remove("/usr/local/bin/pkg-config")
-            os.remove("/usr/local/share/aclocal/pkg.m4")
+            os.chdir(self.cwd)
+            subprocess.call(["make", "uninstall"])
         finally:
             return super().uninstall()
+class prebuild(Prebuild):
+    def __init__(self):
+        self.amd64="ferment://pkgconfig@pkgconfig.tar.gz"
+        self.arm64=self.amd64
+    def install(self):
+        os.chdir(self.cwd)
+        subprocess.call(["make", "install", f"-j{os.cpu_count()}"])
