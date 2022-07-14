bc374a54b10e2847e99612da82a6237319eb5f11
NotTimIsReal <alastairrintumathew@gmail.com>
2022-07-14 18:13:17 +1000

    added removewatermark to prebuild

--- index.py
+++ index.py
@@ -77,4 +77,24 @@ class Prebuild():
         return bool
     def uninstall(self):
         return bool
+    def removeTMPWaterMark(self, pkg:str)->None:
+        originalcontent=open("Makefile", "r+").read()
+        while(f"/private/tmp/fermenter/{pkg}" in originalcontent):
+            originalcontent=originalcontent.replace(f"/private/tmp/fermenter/{pkg}", self.cwd+f"/")
+            originalcontent=originalcontent.replace(f"/tmp/fermenter/{pkg}", self.cwd+f"/")
+            originalcontent=originalcontent.replace("install: all install-bin install-hdrs install-lib install-data", "install: install-bin install-hdrs install-lib install-data")
+            open("Makefile", "w").write(originalcontent)
+            originalcontent=open("Makefile", "r+").read()
+        builddir=os.listdir("build")
+        #buildir should only contains files with the .d extention
+        builddir=[x for x in builddir if x.endswith(".d")]
+        for x in builddir:
+            ogcontent=open(f"build/{x}", "r+").read()
+            ogcontent=ogcontent.replace(f"/private/tmp/fermenter/{pkg}", self.cwd+f"/")
+            open("build/"+x, "w").write(ogcontent)
+            ogcontent=open(f"build/{x}", "r+").read()
+        originalcontent=open("libtool", "r+").read()
+        originalcontent=originalcontent.replace(f"/private/tmp/fermenter/{pkg}", self.cwd+f"/")
+        originalcontent=originalcontent.replace(f"/tmp/fermenter/{pkg}", self.cwd+f"/")
+        open("libtool", "w").write(originalcontent)
 
