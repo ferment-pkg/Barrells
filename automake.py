f31013dc5812fd70a1df1c49dfbddff9423eab79
NotTimIsReal <alastairrintumathew@gmail.com>
2022-07-15 15:51:37 +1000

    automake prebuild doens't work so sticking with manual compilation

--- automake.py
+++ automake.py
@@ -11,33 +11,23 @@ class automake(Barrells):
         self.git=False
         self.description="Automake -- Makefile generator"
         self.dependencies=["autoconf"]
-        self.prebuild=prebuild()
     def install(self) -> bool:
-        os.chdir(self.cwd)
-        os.environ["PERL"]="/usr/bin/perl"
-        subprocess.run(["sh","./configure", f"--prefix=/usr/local"], timeout=1200)
-        subprocess.call(["make"])
-        #wait a second for the make to finish
-        sleep(1)
-        subprocess.call(["make","install"], timeout=120)
-        super().install()
+        with open(f"{self.cwd}/automake-build.log", "a") as f:
+            os.chdir(self.cwd)
+            env=os.environ.copy()
+            env["CC"]="clang"
+            env["CXX"]="clang++"
+            subprocess.call(["sh","./configure", f"--prefix=/usr/local"], stdout=f, stderr=f, env=env)
+            sleep(1)
+            subprocess.call(["make"], stdout=f, stderr=f)
+            subprocess.call(["make","install"],stdout=f, stderr=f)
+            super().install()
+    def test(self):
+        subprocess.call(["automake","--version"])
     def uninstall(self) -> bool:
         try:
             os.chdir(self.cwd)
             subprocess.call(["make","uninstall"])
         finally:
             return super().uninstall()
-    def build(self):
-        with open(f"{self.cwd}/automake-build.log", "a") as stdout:
-            os.chdir(self.cwd)
-            subprocess.call(["sh","configure", "--prefix=/usr/local/"], stdout=stdout, stderr=stdout)
-            subprocess.call(["make", f"-j{os.cpu_count()}"],  stdout=stdout, stderr=stdout)
 
-class prebuild(Prebuild):
-    def __init__(self):
-        self.amd64="ferment://automake@automake.tar.gz"
-        self.arm64="ferment://automake@automake.tar.gz"
-    def install(self):
-        os.chdir(self.cwd)
-        self.removeTMPWaterMark("automake", ["t/wrap/aclocal-1.14", "t/wrap/aclocal.in", "t/wrap/automake-1.14", "t/wrap/automake.in"])
-        subprocess.call(["make", "install", f"-j{os.cpu_count()}"])
