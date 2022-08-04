3963f509de6e3eab944eb9aa96f6cb55020d9df8
NotTimIsReal <alastairrintumathew@gmail.com>
2022-08-04 17:45:15 +1000

    new pyenv build

--- pyenv.py
+++ pyenv.py
@@ -13,6 +13,7 @@ class pyenv(Barrells):
         self.version="2.3.2"
         self.lib = False
         self.dependencies = ["automake"]
+        self.caveats="Run source ~/.zshrc to start using pyenv"
 
     def install(self):
         os.chdir(self.cwd)
@@ -21,29 +22,26 @@ class pyenv(Barrells):
         self.editRC("#pyenv INIT - NO EDIT")
         self.SetPVar("PYENV_ROOT", self.cwd)
         self.EditPath("$PYENV_ROOT/bin")
-        self.editRC("eval $(pyenv init -)")
+        self.editRC('eval "$(pyenv init -)"')
         self.editRC("#pyenv EXIT - NO EDIT")
     def uninstall(self) -> bool:
-        try:
-            home=os.getenv("HOME")
-            f=open(f"{home}/.zshrc", "r")
-            f=f.read()
-            content=f.split("\n")
-            init:int
-            exitHash:int
-            for index in content:
-                if content[index] =="#pyenv INIT - NO EDIT":
-                    init=index
-                    continue
-                if content [index]=="#pyenv EXIT - NO EDIT":
-                    exitHash=index
-                    continue
-            del content[init]
-            del content[exitHash]
-            f=open(f"{home}/.zshrc", "w")
-            f.write(content)
-        finally:
-            return True
-        self.editRC("eval $(pyenv init -")
+        home=os.getenv("HOME")
+        f=open(f"{home}/.zshrc", "r")
+        f=f.read()
+        content=f.split("\n")
+        start=0
+        endHash=0
+        for index in range(len(content)):
+            if content[index]=="#pyenv INIT - NO EDIT":
+                start=index
+            if content[index]=="#pyenv EXIT - NO EDIT":
+                endHash=index
+        if start==0 and endHash==0:
+            return super().uninstall()
+        #remove
+        del content[start:endHash+1]
+        f=open(f"{home}/.zshrc", "w")
+        f.write("\n".join(content))
+        return super().uninstall()
 
 
