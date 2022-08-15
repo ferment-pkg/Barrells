76d6de28d5dad908314e567d3e2103ea37031b7a
NotTimIsReal <alastairrintumathew@gmail.com>
2022-08-15 19:31:48 +1000

    sdl2

--- sdl2.py
+++ sdl2.py
@@ -34,12 +34,13 @@ class sdl2(Barrells):
             import os
             os.chdir(self.cwd)
             import subprocess
-            args=["--prefix=/usr/local/", *self.args]
+            args=["--prefix=build", *self.args]
             env=os.environ.copy()
             env["CC"]=f"{self.cwd}/build-scripts/clang-fat.sh"
             env["CXX"]=f"{self.cwd}/build-scripts/clang++-fat.sh"
             subprocess.run(["sh", "./configure", *args], env=env, stdout=sys.stdout, stderr=sys.stdout)
             subprocess.run(["make", f"-j{os.cpu_count()}"], stdout=sys.stdout, stderr=sys.stdout)
+            subprocess.call(["make", "install"], stdout=sys.stdout, stderr=sys.stdout)
     def uninstall(self) -> bool:
         os.chdir(self.cwd)
         subprocess.call(["make", "uninstall"])
@@ -57,28 +58,4 @@ class prebuild(Prebuild):
        self.arm64="ferment://sdl2@sdl2.tar.gz"
     def install(self):
         os.chdir(self.cwd)
-        originalcontent=open("Makefile", "r+").read()
-        #     content=f.read()
-        #     content=content.replace("/private/tmp/fermenter/sdl2/", self.cwd)
-        #     content=content.replace("/tmp/fermenter/sdl2/", self.cwd)
-        #     f.write(content)
-        #     f.close()
-        while("/private/tmp/fermenter/sdl2" in originalcontent):
-            originalcontent=originalcontent.replace("/private/tmp/fermenter/sdl2", self.cwd+"/")
-            originalcontent=originalcontent.replace("/tmp/fermenter/sdl2", self.cwd+"/")
-            originalcontent=originalcontent.replace("install: all install-bin install-hdrs install-lib install-data", "install: install-bin install-hdrs install-lib install-data")
-            open("Makefile", "w").write(originalcontent)
-            originalcontent=open("Makefile", "r+").read()
-        builddir=os.listdir("build")
-        #buildir should only contains files with the .d extention
-        builddir=[x for x in builddir if x.endswith(".d")]
-        for x in builddir:
-            ogcontent=open(f"build/{x}", "r+").read()
-            ogcontent=ogcontent.replace("/private/tmp/fermenter/sdl2", self.cwd+"/")
-            open("build/"+x, "w").write(ogcontent)
-            ogcontent=open(f"build/{x}", "r+").read()
-        originalcontent=open("libtool", "r+").read()
-        originalcontent=originalcontent.replace("/private/tmp/fermenter/sdl2", self.cwd+"/")
-        originalcontent=originalcontent.replace("/tmp/fermenter/sdl2", self.cwd+"/")
-        open("libtool", "w").write(originalcontent)
-        subprocess.run(["make", "install", f"-j{os.cpu_count()}"], stdout=sys.stdout, stderr=sys.stdout)
+        os.symlink("build/*", "/usr/local/")
