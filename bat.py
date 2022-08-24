8ea2553f5a0c9754e39290e3556dda21b7bf181b
NotTimIsReal <alastairrintumathew@gmail.com>
2022-08-24 09:47:50 +0000

    Bat is now a prebuild

--- bat.py
+++ bat.py
@@ -1,7 +1,8 @@
 # Created With Barrell Tool https://ferment.tk/create
+import os
 import subprocess
 
-from index import Barrells
+from index import Barrells, Prebuild
 
 
 class bat(Barrells):
@@ -10,13 +11,57 @@ class bat(Barrells):
         self.url = "https://github.com/sharkdp/bat/archive/v0.21.0.tar.gz"
         self.git = False
         self.lib = False
-        self.version="0.21"
+        self.version = "0.21"
         self.home = "https://github.com/sharkdp/bat"
         self.dependencies = ["rust"]
+        self.prebuild = prebuild()
 
     def install(self):
-        subprocess.call(["cargo", "install","--locked", "bat"])
+        subprocess.call(["cargo", "install", "--locked", "bat"])
         return super().install()
+
     def uninstall(self) -> bool:
-        subprocess.call(["cargo", "uninstall","--locked", "bat"])
+        os.chdir(self.cwd)
+        if "PREBUILD" in os.listdir():
+            os.remove("bat")
+        else:
+            subprocess.call(["cargo", "uninstall", "--locked", "bat"])
         return super().uninstall()
+
+    def build(self):
+        os.chdir(self.cwd)
+        subprocess.call(
+            ["cargo", "build", "--target=aarch64-apple-darwin", "bat"])
+        subprocess.call(
+            ["cargo", "build", "--target=x86_64-apple-darwin", "bat"])
+        # link with lipo
+        subprocess.call(["lipo", "-create", "-output", "bat",
+                        "target/aarch64-apple-darwin/release/bat", "target/x86_64-apple-darwin/release/bat"])
+
+        contents = os.listdir()
+        # remove bat from contents
+        contents.remove("bat")
+        # move contents to bin
+        for item in contents:
+            os.remove(item)
+        # make empty file called PREBUILD
+        open("PREBUILD", "w").close()
+        return super().build()
+
+    def test(self):
+        out = subprocess.call("bat", "-V")
+        if out > 0:
+            print("False")
+            return False
+        print("True")
+        return True
+
+
+class prebuild(Prebuild):
+    def __init__(self):
+        self.amd64 = "ferment://bat@bat.tar.gz"
+        self.arm64 = "ferment://bat@bat.tar.gz"
+
+    def install(self):
+        os.chdir(self.cwd)
+        os.symlink("bat", "/usr/local/bin/bat")
