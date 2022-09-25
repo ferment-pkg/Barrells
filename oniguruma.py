94b67eb994031bd1c4554152bedfee420c4bf1dd
NotTimIsReal <alastairrintumathew@gmail.com>
2022-09-25 10:21:02 +0000

    added prebuilds

--- oniguruma.py
+++ oniguruma.py
@@ -1,40 +1,61 @@
 import os
 import subprocess
 
-from index import Barrells
+from index import Barrells, Prebuild, universalbinaryenv
 
 
 class oniguruma(Barrells):
     def __init__(self):
-        self.url="https://github.com/kkos/oniguruma/releases/download/v6.9.8/onig-6.9.8.tar.gz"
-        self.git=False
-        self.version-"6.9.8"
-        self.description="Oniguruma is a modern and flexible regular expressions library."
-        self.dependencies=["autoconf", "automake", "libtool"]
-        self.lib=True
+        self.url = "https://github.com/kkos/oniguruma/releases/download/v6.9.8/onig-6.9.8.tar.gz"
+        self.git = False
+        self.version = "6.9.8"
+        self.description = "Oniguruma is a modern and flexible regular expressions library."
+        self.dependencies = ["autoconf", "automake", "libtool"]
+        self.lib = True
+
     def install(self) -> bool:
         os.chdir(self.cwd)
-        subprocess.run(["autoreconf", "-vfi"], timeout=1200)
-        subprocess.run(["sh", "./configure", f"--prefix={self.cwd}/built"], timeout=1200)
-        subprocess.call(["make"], timeout=1200)
-        subprocess.call(["make", "install"], timeout=1200)
-        os.symlink(f"{self.cwd}/built/bin/onig-config", "/usr/local/bin/onig-config")
-        os.symlink(f"{self.cwd}/built/include/oniggnu.h", "/usr/local/include/oniggnu.h")
-        os.symlink(f"{self.cwd}/built/include/onigguruma.h", "/usr/local/include/onigguruma.h")
-        dirs = filter(os.path.isdir, os.listdir(f"{self.cwd}/built/lib"))
-        for f in dirs:
-            os.symlink(f"{self.cwd}/built/lib/{f}", f"/usr/local/lib/{f}")
-        os.symlink(f"{self.cwd}/built/lib/pkgconfig/oniguruma.pc", "/usr/local/lib/pkgconfig/oniguruma.pc")
+        subprocess.run(["autoreconf", "-vfi"])
+        subprocess.run(
+            ["sh", "./configure"])
+        subprocess.call(["make"])
+        subprocess.call(["make", "install"])
 
         return super().install()
+
     def uninstall(self) -> bool:
-        try:
-            os.remove("/usr/local/bin/onig-config")
-            os.remove("/usr/local/include/oniggnu.h")
-            os.remove("/usr/local/include/onigguruma.h")
-            dirs = filter(os.path.isdir, os.listdir(f"{self.cwd}/built/lib"))
-            for f in dirs:
-                os.remove(f"/usr/local/lib/{f}")
-            os.remove("/usr/local/lib/pkgconfig/oniguruma.pc")
-        finally:
-            return super().uninstall()
+        os.chdir(self.cwd)
+        watcher = open(".ferment_watcher", "r", encoding='utf-8')
+        content = watcher.read()
+        watcher.close()
+        content = content.split("\n")
+        for line in content:
+            os.remove(line)
+
+    def build(self) -> bool:
+        os.chdir(self.cwd)
+        subprocess.run(["autoreconf", "-vfi"])
+        subprocess.run(
+            ["sh", "./configure"])
+        subprocess.call(
+            ["make", f"-j{os.cpu_count()}"],  env=universalbinaryenv())
+
+        return super().build()
+
+    def test(self):
+        os.chdir(self.cwd)
+        if subprocess.check_output(["make", "check"]) != 0:
+            print("False")
+            return False
+        print("True")
+        return True
+
+
+class prebuild(Prebuild):
+    def __init__(self):
+        self.amd64 = "ferment://oniguruma@oniguruma.tar.gz"
+        self.arm64 = "ferment://oniguruma@oniguruma.tar.gz"
+
+    def install(self):
+        os.chdir(self.cwd)
+        subprocess.call(["make", "install"])
